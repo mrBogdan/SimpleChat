cb34fb446dcbf9ea34d27cfd70c45ec52ccb7a77
Bogdan Babitskiy <dandan250899@gmail.com>
2018-05-11 07:59:17 +0300

    Create a chat

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+var io      = require('socket.io').listen(server);
+users       = [];
+connections  = [];
+
+server.listen(process.env.PORT || 3000);
+
+console.log('Server running');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    // Disconnect
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Diconnected: %s sockets connected', connections.length);
+    })
+
+    // Send message
+    socket.on('send message', function (data) {
+        io.sockets.emit('new message', {msg: data});
+    })
+
+});

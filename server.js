941843b7a01e142127a84dabdb2665dce798a833
Bogdan Babitskiy <dandan250899@gmail.com>
2018-05-22 08:02:03 +0300

    Almost end

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app     = express();
 var server  = require('http').createServer(app);
 var io      = require('socket.io').listen(server);
-users       = [];
-connections  = [];
+var users       = [];
+var connections  = [];
 
 server.listen(process.env.PORT || 3000);
 
@@ -19,13 +19,27 @@ io.sockets.on('connection', function (socket) {
 
     // Disconnect
     socket.on('disconnect', function (data) {
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Diconnected: %s sockets connected', connections.length);
-    })
+    });
 
     // Send message
     socket.on('send message', function (data) {
-        io.sockets.emit('new message', {msg: data});
-    })
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+
+    // New User
+    socket.on('new user', function (data, callback) {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames() {
+        io.sockets.emit('get users', users);
+    }
 
 });
